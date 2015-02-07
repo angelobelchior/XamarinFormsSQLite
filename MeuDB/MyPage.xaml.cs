a15ae19531b23af25f0262236b4c26b7daa3e131
Angelo Luis <angelobelchior@hotmail.com>
2015-02-07 01:51:16 -0200

    Commit Inicial

---
+++ MeuDB/MyPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace MeuDB
+{
+	public partial class MyPage : ContentPage
+	{
+		public MyPage ()
+		{
+			InitializeComponent ();
+
+			using(var dados = new AcessoDados())
+			{
+				this.Lista.ItemsSource = dados.Listar ();
+			}
+		}
+
+		protected void SalvarClicked(object sender, EventArgs e)
+		{
+			var contato = new Contato {
+				Nome = this.Nome.Text,
+				Email = this.Email.Text,
+				Telefone = this.Telefone.Text
+			};
+
+			using(var dados = new AcessoDados())
+			{
+				dados.Insert (contato);
+				this.Lista.ItemsSource = dados.Listar ();
+			}
+		}
+	}
+}
